f41b3fe8ccae6d2d80bf08b2a8c5e810fe1018f1
unknown <nayefkanaan03@gmail.com>
2021-11-10 04:44:45 +0300

    WIP

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,11 +10,12 @@ VALUES ("Marketing Team Lead", 135000, 1),
         ("DevOps Engineer", 170000, 2),
         ("Accountant", 85000, 5),
         ("Senior Back-End Engineer", 200000, 3),
-        ("Front-End Engineer", 75000, 3);
+        ("Front-End Engineer", 75000, 4);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("George","Costanza", 1, 1),
-        ("Jerry","Seinfeld", 2, null),
-        ("Elaine","Benes", 3, null),
-        ("Cosma","Kramer", 3, 3);
+        ("Jerry","Seinfeld", 2, 2),
+        ("Elaine","Benes", 3, 4),
+        ("Cosma","Kramer", 4, 3);
+
