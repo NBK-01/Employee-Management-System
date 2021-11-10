984a1f48b0e55133a64260164fdbe28b38c10635
unknown <nayefkanaan03@gmail.com>
2021-11-10 04:48:41 +0300

    Updating schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,56 @@
 DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 USE employeeDB;
-
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
-
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT ,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
-
-
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT  ,
-    manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
-    );
+CREATE TABLE employeeDB (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+use employeeDB;
+INSERT INTO department
+    (name)
+VALUES
+    ('Operations'),
+    ('Analystics'),
+    ('Marketing'),
+    ('Executive');
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('General Manager', 11000000, 1),
+    ('Coach', 4000000, 1),
+    ('Team Lead Analyst', 15000000, 2),
+    ('Team Analyst', 8000000, 2),
+    ('Media Manager', 7000000, 3),
+    ('Media Specialist', 3000000, 3),
+    ('CEO', 45000000, 4),
+    ('CEO Assistant', 25000000, 4);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('George', 'Costanza', 1, NULL),
+    ('Jerry', 'Seinfeld', 2, 1),
+    ('Cosma', 'Kramer', 3, NULL),
+    ('Susan', 'Ross', 4, 3),
+    ('Elaine', 'Benes', 5, NULL),
+    ('Ross', 'Geller', 6, 5),
+    ('Monica', 'Geller', 7, NULL),
+    ('Rachel', 'Green', 8, 7);
